a7064a893f5bc2530546b1c6caed75f1cd907c21
silastittes <silas.tittes@gmail.com>
2020-01-29 06:29:45 -0800

    added files for proper job submission

---
+++ submit.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+snakemake --jobs 100 \
+    --rerun-incomplete \
+    --latency-wait 5 \
+    --cluster-config submit.json  \
+    --cluster "sbatch -p {cluster.p} --mem {cluster.mem} --cpus-per-task {cluster.cpus-per-task} --time {cluster.time} --job-name {cluster.name} -e {cluster.e} -o {cluster.o}"
+
+
