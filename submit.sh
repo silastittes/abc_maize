562e7f1fd6a57949e321c0289b531f85daaa1b20
silastittes <silas.tittes@gmail.com>
2020-04-15 20:30:02 -0700

    changes to window size, priors, mutation and recombination rate

--- submit.sh
+++ submit.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
 
 snakemake --jobs 200 \
+    --batch all=1/4 \
     --rerun-incomplete \
     --latency-wait 60 \
     --cluster-config submit.json  \
     --cluster "sbatch -p {cluster.p} --mem {cluster.mem} --cpus-per-task {cluster.cpus-per-task} --time {cluster.time} --job-name {cluster.name} -e {cluster.e} -o {cluster.o}"
 
+#snakemake --jobs 200 \
+#    --batch all=2/4 \
+#    --rerun-incomplete \
+#    --latency-wait 60 \
+#    --cluster-config submit.json  \
+#    --cluster "sbatch -p {cluster.p} --mem {cluster.mem} --cpus-per-task {cluster.cpus-per-task} --time {cluster.time} --job-name {cluster.name} -e {cluster.e} -o {cluster.o}"
+
+#snakemake --jobs 200 \
+#    --batch all=3/4 \
+#    --rerun-incomplete \
+#    --latency-wait 60 \
+#    --cluster-config submit.json  \
+#    --cluster "sbatch -p {cluster.p} --mem {cluster.mem} --cpus-per-task {cluster.cpus-per-task} --time {cluster.time} --job-name {cluster.name} -e {cluster.e} -o {cluster.o}"
+
+#snakemake --jobs 200 \
+#    --batch all=4/4 \
+#    --rerun-incomplete \
+#    --latency-wait 60 \
+#    --cluster-config submit.json  \
+#    --cluster "sbatch -p {cluster.p} --mem {cluster.mem} --cpus-per-task {cluster.cpus-per-task} --time {cluster.time} --job-name {cluster.name} -e {cluster.e} -o {cluster.o}"
 
