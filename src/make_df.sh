e1fc4a25d11d7d9f11ce9454b566a642f1c2394e
silastittes <silas.tittes@gmail.com>
2020-04-28 14:25:50 -0700

    updates for cds version of sims

--- src/make_df.sh
+++ src/make_df.sh
@@ -1,9 +1,9 @@
 
-#for i in `ls abc_out/*Na__1000_*`; do grep -h -B2 "SFS:" abc_out/$i | grep "SFS:" | cut -d ":" -f2- | sed 's/^ //g'; done > pop_sfs.txt &
+for i in `ls abc_out/*Na__1000_*`; do grep -h -B2 "SFS:" $i | grep "SFS:" | cut -d ":" -f2- | sed 's/^ //g'; done > pop_sfs.txt &
 
-#for i in `ls abc_out/*Na__1000_*`; do grep -h -B2 "SFS:" abc_out/$i | grep -h -A1 "#Na" | grep -v "\-\-" | grep -v "#Na"; done > pop.txt
+for i in `ls abc_out/*Na__1000_*`; do grep -h -B2 "SFS:" $i | grep -h -A1 "#Na" | grep -v "\-\-" | grep -v "#Na"; done > pop.txt
 
 
-for i in `ls abc_out/*Na__200_*`; do grep -h -B2 "SFS:" $i | grep "SFS:" | cut -d ":" -f2- | sed 's/^ //g'; done > pop200_sfs.txt &
+#for i in `ls abc_out/*Na__200_*`; do grep -h -B2 "SFS:" $i | grep "SFS:" | cut -d ":" -f2- | sed 's/^ //g'; done > pop200_sfs.txt &
 
-for i in `ls abc_out/*Na__200_*`; do grep -h -B2 "SFS:" $i | grep -h -A1 "#Na" | grep -v "\-\-" | grep -v "#Na"; done > pop200.txt
+#for i in `ls abc_out/*Na__200_*`; do grep -h -B2 "SFS:" $i | grep -h -A1 "#Na" | grep -v "\-\-" | grep -v "#Na"; done > pop200.txt
