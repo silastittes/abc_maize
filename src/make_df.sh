8d536265b020b7aed7409a1fb40bcfba4f82f5ee
silastittes <silas.tittes@gmail.com>
2020-04-30 21:02:58 -0700

    important population size bug fix. new version of slimulation to simulation SVs across whole region, keeping mutation rate the proportional inside and outside coding regions

--- src/make_df.sh
+++ src/make_df.sh
@@ -1,7 +1,7 @@
 
-for i in `ls abc_out/*Na__1000_*`; do grep -h -B2 "SFS:" $i | grep "SFS:" | cut -d ":" -f2- | sed 's/^ //g'; done > pop_sfs.txt &
+for i in abc_out/*Na__1000_*; do grep -h -B2 "SFS:" $i | grep "SFS:" | cut -d ":" -f2- | sed 's/^ //g'; done > pop_sfs.txt &
 
-for i in `ls abc_out/*Na__1000_*`; do grep -h -B2 "SFS:" $i | grep -h -A1 "#Na" | grep -v "\-\-" | grep -v "#Na"; done > pop.txt
+for i in abc_out/*Na__1000_*; do grep -h -B2 "SFS:" $i | grep -h -A1 "#Na" | grep -v "\-\-" | grep -v "#Na"; done > pop.txt
 
 
 #for i in `ls abc_out/*Na__200_*`; do grep -h -B2 "SFS:" $i | grep "SFS:" | cut -d ":" -f2- | sed 's/^ //g'; done > pop200_sfs.txt &
