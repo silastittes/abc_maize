8a5be9916590fe91e63609f20561c07cfde70c89
silastittes <silas.tittes@gmail.com>
2020-07-12 09:52:41 -0700

    more edits to compare fixed versus predicted sv mu

--- src/make_df.sh
+++ src/make_df.sh
@@ -1,7 +1,9 @@
 
-for i in abc_out/*Na__1000_*; do grep -h -B2 "SFS:" $i | grep "SFS:" | cut -d ":" -f2- | sed 's/^ //g'; done > pop_sfs.txt &
+for i in abc_out/*Na__1000_*; do grep -h -B2 "SFS:" $i | grep "SFS:" | cut -d ":" -f2- | sed 's/^ //g'; done > pop_sfs_fixedmu.txt &
+for i in abc_out/*Na__1000_*; do grep -h -B2 "SFS:" $i | grep -h -A1 "#Na" | grep -v "\-\-" | grep -v "#Na"; done > pop_fixedmu.txt
 
-for i in abc_out/*Na__1000_*; do grep -h -B2 "SFS:" $i | grep -h -A1 "#Na" | grep -v "\-\-" | grep -v "#Na"; done > pop.txt
+#for i in abc_out_FULL/*Na__1000_*; do grep -h -B2 "SFS:" $i | grep "SFS:" | cut -d ":" -f2- | sed 's/^ //g'; done > pop_sfs.txt &
+#for i in abc_out_FULL/*Na__1000_*; do grep -h -B2 "SFS:" $i | grep -h -A1 "#Na" | grep -v "\-\-" | grep -v "#Na"; done > pop.txt
 
 
 #for i in `ls abc_out/*Na__200_*`; do grep -h -B2 "SFS:" $i | grep "SFS:" | cut -d ":" -f2- | sed 's/^ //g'; done > pop200_sfs.txt &
